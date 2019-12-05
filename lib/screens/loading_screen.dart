bac82d31602e7585545f2cd9fbc78e3b3a9d6275
ThiagoFTelles <atendimento.telles@gmail.com>
2019-12-05 12:58:42 -0300

    aula 136

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,6 +9,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location theLocation = Location();
+
   @override
   void initState() {
     super.initState();
@@ -14,13 +18,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    await theLocation.getCurrentLocation();
+    Map<String, double> usersCurrentLocation = {
+      'lat': theLocation.latitude,
+      'long': theLocation.longitude
+    };
+    print(usersCurrentLocation);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
+
     return Scaffold();
   }
 }
