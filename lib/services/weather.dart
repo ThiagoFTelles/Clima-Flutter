2594e7ae287be2ee00375458ab6144fcd077ae3b
ThiagoFTelles <atendimento.telles@gmail.com>
2019-12-12 14:29:43 -0300

    aula final

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -6,6 +6,15 @@ const apiKey = 'c0ea72293e7bba98fccec82d038faf3c';
 const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future getCityWeather(String cityName) async {
+    var url = '$openWeatherMapUrl?q=$cityName&appid=$apiKey&units=metric';
+
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location theLocation = Location();
 
